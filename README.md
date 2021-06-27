79a7b5b8861a5fc56258a7fa96e21af19e712c40
Alex Kehayias <alex@mosey.so>
2021-06-27 13:12:35 -0700

    Add config for running tests

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
+# About
+
+In this interview we will be building a datefield input from scratch.
+
 
 ## Running the dev server
 
 ```
 npm install && npm start
+```
+
+
+## Running tests
+
+```
+npm test
 ```
---
+++ test/index.spec.ts
@@ -0,0 +1,13 @@
+import * as mod from '../src/index';
+import { expect } from 'chai';
+import 'mocha';
+
+
+describe(
+  'My Test',
+  () => {
+    it('does something', () => {
+      expect(1).to.equal(1);
+    })
+  }
+);
